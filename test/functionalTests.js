c064c39b9e38e9b4772cc80df2f46f059f30dc7e
Emanuele DelBono <emanuele@codiceplastico.com>
2014-12-06 10:39:17 +0100

    removed mocha-request in favor of request

--- test/functionalTests.js
+++ test/functionalTests.js
@@ -1,5 +1,4 @@
 var request = require('request');
-var http = require('request-mocha')(request);
 var server = require('../lib/server');
 var expect = require('chai').expect;
 
@@ -8,18 +7,21 @@ describe('A server receiving a request', function () {
     server.start();
   });
 
-  http.save({
-    method: 'POST',
-    url: 'http://localhost:8000/login',
-    form: {
+  it('Should login a new user', function () {
+
+    var form = {
       nickname: 'mocha test',
       coords: [4,5]
-    }
-  });
-  
-  it('Should login a new user', function () {
-    var body = JSON.parse(this.body);
-    expect(this.res.statusCode).to.equal(200);
-    expect(body.uuid).to.not.be.undefined();
+    };
+
+    request.post('http://localhost:8000/login', {form: form} , function (err, res, body){
+      var body = JSON.parse(this.body);
+      expect(this.res.statusCode).to.equal(200);
+      expect(body.uuid).to.not.be.undefined();  
+
+      done();
+    });
+
+    
   });
 });
